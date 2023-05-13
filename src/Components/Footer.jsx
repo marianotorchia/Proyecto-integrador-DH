5fc5732d8ba1e902b2e99d38c997649cd29bd405
marianotorchia <torchia.mariano27@gmail.com>
2023-05-12 21:52:38 -0300

    segundo commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,11 @@
 
 const Footer = () => {
     return (
-      <footer>
-          <p>© 2023  Music House</p>
+      <footer className="footer">
+          <p>© 2023 
+            <a className='Title-bold'> M </a><a className='Title-semibold'>u </a><a className='Title-Medium'>s </a><a className='Title-regular'>i </a><a className='Title-light'>c </a>
+            <a className='Title-regular'>House</a>
+          </p>
       </footer>
     )
   }
